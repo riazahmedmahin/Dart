463d3a519f40d8835fb29a5ee6f18f2adf4e3d21
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2023-08-02 21:06:57 +0600

    Create Module_1.dart

---
+++ Module_1/Module_1.dart
@@ -0,0 +1,26 @@
+void main() {
+
+  const int a = 10;
+  const int b = 4;
+
+  // Addition
+  int sum = a + b;
+  print("Addition: $a + $b = $sum");
+
+  // Subtraction
+  int Subtraction = a - b;
+  print("Subtraction: $a - $b = $Subtraction");
+
+  // Multiplication
+  int Multiplication = a * b;
+  print("Multiplication: $a * $b = $Multiplication");
+
+  // Division
+  double Division = a / b;
+  print("Division: $a / $b = $Division");
+
+  // Modulus
+  int Modulus = a % b;
+  print("Modulus: $a % $b = $Modulus");
+
+}
