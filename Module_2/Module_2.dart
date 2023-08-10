16a181f1eacb2a515f1004fab0eaa7055945d5c0
Riaz Ahmed Mahin <71597653+riazahmedmahin@users.noreply.github.com>
2023-08-10 20:22:41 +0600

    Update Module_2.dart

--- Module_2/Module_2.dart
+++ Module_2/Module_2.dart
@@ -1,1 +1,22 @@
+void main() {
+  
+  List<int> grades = [85, 92, 78, 65, 88, 72];
 
+  int total = 0;
+  for (int grade in grades) {
+    total += grade;
+  }
+
+  double average = total / grades.length;
+  int roundedAverage = average.round();
+
+  print(" Student's average grade: $average");
+  print("Rounded average: $roundedAverage");
+
+  if (roundedAverage >= 70) {
+    print("Passed");
+  } else {
+    print("Failed");
+  }
+  
+}
